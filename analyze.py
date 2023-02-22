5a4e5c300e0cdf32b0be3d5cbc12f2a6ec75f0aa
philtpatton <pattonp@hawaii.edu>
2023-02-22 08:54:59 -1000

    add some catches for missing json files

--- analyze.py
+++ analyze.py
@@ -19,21 +19,32 @@ def summarize_json():
 
     for catalog in catalog_ids:
 
+        results_dir = f'results/{args.scenario}/{catalog}'
+
         config_path = f'config/{args.scenario}/{catalog}.yaml'
         cfg = load_config(config_path, "config/default.yaml")
         trial_count = cfg.trial_count
 
-        summary_dir = f'results/{args.scenario}/{catalog}/summary'
+        summary_dir = f'{results_dir}/summary'
         if not os.path.isdir(summary_dir):
             os.mkdir(summary_dir)
 
-        for trial in trial_count:
-            path = f'results/{args.scenario}/{catalog}/trial_{trial}.json'
-            idata = az.from_json(path)  
+        i = 0
+        for trial in range(trial_count):
+            try:
+                path = f'{results_dir}/trial_{trial}.json'
+                idata = az.from_json(path)
+            except OSError:
+                print(f'{path} is missing')
+                i += 1
+                continue
 
             summary = az.summary(idata)
             out_file = f'{summary_dir}/trial_{trial}.csv'
             summary.to_csv(out_file)
+
+        if i != 0:
+            print(f'{catalog} had {i} missing json files')
 
     return None
 
