14d128783b9fa33f0c37397304a11109bdb8d547
philtpatton <pattonp@hawaii.edu>
2023-02-22 08:34:36 -1000

    add summaries to subfolder

--- analyze.py
+++ analyze.py
@@ -1,6 +1,8 @@
 import numpy as np
 import arviz as az
+
 import argparse
+import os
 
 from config import load_config, Config
 
@@ -21,12 +23,16 @@ def summarize_json():
         cfg = load_config(config_path, "config/default.yaml")
         trial_count = cfg.trial_count
 
+        summary_dir = f'/results/{args.scenario}/{catalog}/summary'
+        if not os.path.isdir(summary_dir):
+            os.makedirs(summary_dir)
+
         for trial in trial_count:
             path = f'/results/{args.scenario}/{catalog}/trial_{trial}.json'
             idata = az.from_json(path)  
 
             summary = az.summary(idata)
-            out_file = f'results/{args.scenario}/{catalog}/trial_{trial}.csv'
+            out_file = f'{summary_dir}/trial_{trial}.csv'
             summary.to_csv(out_file)
 
     return None
