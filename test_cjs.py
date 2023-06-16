5caa5f8a22814ad24d981d8a121c781258764f76
Philip Patton <pattonp@hawaii.edu>
2023-06-16 12:02:12 +0200

    test with dipper data

--- test_cjs.py
+++ test_cjs.py
@@ -55,94 +55,106 @@ def test_mle():
 
 def test_bayes_estimator():
 
-    cs = Simulator(**debug_kwargs)   
-    results = cs.simulate()
+    # cs = Simulator(**debug_kwargs)   
+    # results = cs.simulate()
 
-    be = BayesEstimator(results['capture_history'])
+    data = np.array(
+        [[11, 2 , 0  ,0  ,0  ,0 , 9 ],
+        [0 , 24, 1  ,0  ,0  ,0 , 35],
+        [0 , 0 , 34 ,2  ,0  ,0 , 42],
+        [0 , 0 , 0  ,45 ,1  ,2 , 32],
+        [0 , 0 , 0  ,0  ,51 ,0 , 37],
+        [0 , 0 , 0  ,0  ,0  ,52, 46]])    
+
+    be = BayesEstimator(data)
     model = be.compile()
 
     with  model:
         idata = pm.sample(**sample_kwargs)
 
-    summary = az.summary(idata, var_names='phi')
+    summary = az.summary(idata)
 
-    phi_true = debug_kwargs['phi']
+    phi_summary = summary.loc[summary.index.str.contains('phi')]
+
+    phi_mle = np.array([0.626, 0.454, 0.478, 0.624, 0.608, 0.583])
+    phi_posterior_mean = phi_summary['mean'].values
+    assert np.allclose(phi_mle, phi_posterior_mean, rtol=0.05)
 
     # check coverage
-    phi_low = summary['hdi_3%']
-    phi_high = summary['hdi_97%']
-    is_between = (phi_true > phi_low) & (phi_true < phi_high)
+    phi_low = phi_summary['hdi_3%']
+    phi_high = phi_summary['hdi_97%']
+    is_between = (phi_mle > phi_low) & (phi_mle < phi_high)
     assert all(is_between)
 
     # check bias
-    abs_bias = (phi_true - summary['mean']).abs()
+    # abs_bias = (phi_true - summary['mean']).abs()
 
-    assert all(abs_bias < 0.2)
+    # assert all(abs_bias < 0.2)
 
-#     # compute tukey-statistic
+# #     # compute tukey-statistic
 
-#     # expected values of cells 
-#     T = debug_kwargs['T']
-#     occasion_count = T
-#     interval_count = T - 1
+# #     # expected values of cells 
+# #     T = debug_kwargs['T']
+# #     occasion_count = T
+# #     interval_count = T - 1
 
-#     intervals = np.arange(interval_count)
-#     occasions = np.arange(occasion_count)
+# #     intervals = np.arange(interval_count)
+# #     occasions = np.arange(occasion_count)
 
-#     i = np.reshape(intervals, (interval_count, 1))
-#     j = np.reshape(occasions, (1, occasion_count))
-#     not_cap_visits = np.clip(j - i - 1, 0, np.inf)[:, 1:]
+# #     i = np.reshape(intervals, (interval_count, 1))
+# #     j = np.reshape(occasions, (1, occasion_count))
+# #     not_cap_visits = np.clip(j - i - 1, 0, np.inf)[:, 1:]
 
-#     # p_not_cap = np.triu((1 - p) ** not_cap_visits)
+# #     # p_not_cap = np.triu((1 - p) ** not_cap_visits)
 
-#     # with model:
-#     #     pm.sample_posterior_predictive(idata, extend_inferencedata=True)
+# #     # with model:
+# #     #     pm.sample_posterior_predictive(idata, extend_inferencedata=True)
 
-#     # idata.posterior_predictive
+# #     # idata.posterior_predictive
 
-#     stacked = az.extract(idata)
+# #     stacked = az.extract(idata)
 
-#     sum = summarize_individual_history(results['capture_history'])
+# #     sum = summarize_individual_history(results['capture_history'])
 
-#     # M array (add zero row to for compatability below)
-#     M = sum['m_array']
-#     M = np.insert(M, 0, 0, axis=1)
+# #     # M array (add zero row to for compatability below)
+# #     M = sum['m_array']
+# #     M = np.insert(M, 0, 0, axis=1)
 
-#     # vectorize the recapture counts and probabilities 
-#     upper_triangle_indices = np.triu_indices_from(M[:, 1:])
-#     recapture_counts = M[:, 1:][upper_triangle_indices]
-#     # recapture_probabilities = nu[upper_triangle_indices]
+# #     # vectorize the recapture counts and probabilities 
+# #     upper_triangle_indices = np.triu_indices_from(M[:, 1:])
+# #     recapture_counts = M[:, 1:][upper_triangle_indices]
+# #     # recapture_probabilities = nu[upper_triangle_indices]
 
-#     p_samples = stacked.p.values
-#     phi_samples = stacked.phi.values
+# #     p_samples = stacked.p.values
+# #     phi_samples = stacked.phi.values
 
-#     p = np.repeat(p_samples[0], interval_count)
-#     phi = phi_samples[:,0]
-#     pbar = 1 - p
+# #     p = np.repeat(p_samples[0], interval_count)
+# #     phi = phi_samples[:,0]
+# #     pbar = 1 - p
     
-#     ni = interval_count
-#     nj = interval_count
+# #     ni = interval_count
+# #     nj = interval_count
 
-#     # q defines the multinomial cell probabilities
-#     q = np.zeros((ni, nj + 1))
+# #     # q defines the multinomial cell probabilities
+# #     q = np.zeros((ni, nj + 1))
 
-#     # fill the diagonal elemens 
-#     diag_indices = np.diag_indices(ni)
-#     q[diag_indices] = phi * p
+# #     # fill the diagonal elemens 
+# #     diag_indices = np.diag_indices(ni)
+# #     q[diag_indices] = phi * p
     
-#     # and the off diagonal elements
-#     for i in range(ni - 1):
-#         for j in range(i + 1, nj):
-#             q[i, j] = np.prod(phi[i:j+1]) * np.prod(pbar[i+1:j+1]) * p[j + 1]
+# #     # and the off diagonal elements
+# #     for i in range(ni - 1):
+# #         for j in range(i + 1, nj):
+# #             q[i, j] = np.prod(phi[i:j+1]) * np.prod(pbar[i+1:j+1]) * p[j + 1]
 
-#     # Calculate the disappearing animal probabilities
-#     for i in range(ni):
-#         q[i, nj] = 1 - np.sum(q[i, i:nj])
+# #     # Calculate the disappearing animal probabilities
+# #     for i in range(ni):
+# #         q[i, nj] = 1 - np.sum(q[i, i:nj])
 
-#     rng = np.random.Generator()
-#     r = M.sum(axis=1)
+# #     rng = np.random.Generator()
+# #     r = M.sum(axis=1)
 
-#     for i in range(ni):
-#         m_new = rng.multinomial(r[i], q[i,:])
+# #     for i in range(ni):
+# #         m_new = rng.multinomial(r[i], q[i,:])
 
-#     print(m_new)
+# #     print(m_new)
